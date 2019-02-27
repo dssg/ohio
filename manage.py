118e5a901dcde78f79a758f9585e0a121a6d44fa
Jesse London <jesselondon@gmail.com>
2019-02-27 17:12:25 -0600

    build and release commands, (and wheel requirement)

--- manage.py
+++ manage.py
@@ -1,12 +1,39 @@
 from argparse import REMAINDER
 
-from argcmdr import LocalRoot, local
+from argcmdr import LocalRoot, local, localmethod
 
 
 class Management(LocalRoot):
     """management commands for ohio development"""
 
+    package_name = 'ohio'
+
     @local('remainder', metavar='additional arguments', nargs=REMAINDER)
     def test(self, args):
         """test the codebase"""
         return (self.local.FG, self.local['tox'][args.remainder])
+
+    @local
+    def bump(self):
+        """increment package version (UNIMPLEMENTED)"""
+        raise NotImplementedError
+
+    @local
+    def build(self):
+        """build the python distribution"""
+        return (self.local.FG, self.local['python'][
+            'setup.py',
+            'sdist',
+            'bdist_wheel',
+        ])
+
+    @localmethod('versions', metavar='version', nargs='*',
+                 help="specific version(s) to upload (default: all)")
+    def release(self, args):
+        """upload distribution(s) to pypi"""
+        if args.versions:
+            targets = [f'dist/{self.package_name}-{version}*'
+                       for version in args.versions]
+        else:
+            targets = [f'dist/{self.package_name}-*']
+        return (self.local.FG, self.local['twine']['upload'][targets])
