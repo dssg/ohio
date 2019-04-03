d5207dc301c39a1266841b886d9dfc15c4bdefbf
Jesse London <jesselondon@gmail.com>
2019-04-03 11:12:47 -0500

    removed profilers of experimental feature `iter_size`

--- prof/profilers/copy_from.py
+++ prof/profilers/copy_from.py
@@ -54,38 +54,6 @@ def ohio_pg_copy_from_10(engine, query):
 @mprof
 @time
 @sizecheck
-def ohio_pg_copy_from_10_10(engine, query):
-    """pg_copy_from(buffer_size=10, iter_size=10) {COPY → PipeTextIO → pandas.read_csv}"""
-    return pandas.DataFrame.pg_copy_from(query,
-                                         engine,
-                                         parse_dates=['as_of_date'],
-                                         buffer_size=10,
-                                         iter_size=10)
-
-
-@profiler
-@loadquery
-@loaddata
-@dtypes
-@mprof
-@time
-@sizecheck
-def ohio_pg_copy_from_10_10000(engine, query):
-    """pg_copy_from(buffer_size=10, iter_size=10_000) {COPY → PipeTextIO → pandas.read_csv}"""
-    return pandas.DataFrame.pg_copy_from(query,
-                                         engine,
-                                         parse_dates=['as_of_date'],
-                                         buffer_size=10,
-                                         iter_size=10_000)
-
-
-@profiler
-@loadquery
-@loaddata
-@dtypes
-@mprof
-@time
-@sizecheck
 def ohio_pg_copy_from_10_stream_results(engine, query):
     """pg_copy_from(buffer_size=10) {stream_results | COPY → PipeTextIO → pandas.read_csv}"""
     engine1 = sqlalchemy.create_engine(engine.url, execution_options=dict(stream_results=True,
