6d7521a3b865a25981216a9b313d66f392876ce5
Jesse London <jesselondon@gmail.com>
2019-04-18 17:02:40 -0500

    add src to python path when building docs
    previously assumed environment was so configured

--- doc/conf.py
+++ doc/conf.py
@@ -10,9 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import pathlib
+import sys
+
+sys.path.insert(0, str(pathlib.Path.cwd().parent / 'src'))
 
 
 # -- Project information -----------------------------------------------------
